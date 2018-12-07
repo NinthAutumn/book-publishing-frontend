f02630a4b68d9e67c64e2e89e5661eed99e8755a
Arif Iwamoto <arif.iwamoto@gmail.com>
2018-12-08 01:09:57 +0700

    more layout styling

--- layouts/Navbar/Horizontal.vue
+++ layouts/Navbar/Horizontal.vue
@@ -5,6 +5,7 @@
         <i class="el-icon-menu"></i>
       </div>
       <SearchBar class="searchbar"></SearchBar>
+      <img src="http://placehold.jp/40x40.png" style="border-radius:100px;  margin-right: 10px;">
     </nav>
   </div>
 </template>
@@ -23,6 +24,10 @@ nav {
   width: 100%;
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  -webkit-box-shadow: 0px 2px 2px 0px rgba(217, 208, 219, 1);
+  -moz-box-shadow: 0px 2px 2px 0px rgba(217, 208, 219, 1);
+  box-shadow: 0px 2px 2px 0px rgba(217, 208, 219, 1);
 }
 ul {
   list-style: none;
@@ -33,5 +38,11 @@ ul {
 .el-icon-menu {
   font-size: 32px;
   color: #c9b1f5;
+}
+.searchbar {
+}
+
+.left-menu {
+  margin-left: 10px;
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -14,4 +14,7 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 </style>
