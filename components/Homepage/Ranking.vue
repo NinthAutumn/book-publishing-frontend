082f18ad4a3f8381e55544d5c50498cd1abee94b
Arif Iwamoto <arif.iwamoto@gmail.com>
2019-02-23 23:53:36 +0900

    ranking in homepage

--- components/Homepage/Ranking.vue
+++ components/Homepage/Ranking.vue
@@ -8,7 +8,7 @@
           <div class="tab-ranking__tab">一ヶ月</div>
           <div class="tab-ranking__tab">一年</div>
         </div>
-        <div class="tab-ranking__main">
+        <div class="tab-ranking__main flex">
           <div class="tab-ranking__first flex">
             <div class="tab-ranking__first__cover">
               <div class="tab-ranking__first__index flex flex--align flex--around">1</div>
@@ -22,18 +22,24 @@
               <div class="tab-ranking__first__synopsis" v-text="books[0]._id.book[0].synopsis"></div>
             </div>
           </div>
-          <div
-            class="tab-ranking__book"
-            v-for="(book, index) in books"
-            :key="index"
-            :class="{'tab-ranking__book--second': index === 1,'tab-ranking__book--third':index===2}"
-          >
-            <div class="tab-ranking__item" v-if="index === 0"></div>
-            <div class="tab-ranking__item" v-else>
-              <div class="tab-ranking__cover"></div>
-              <div class="tab-ranking__meta">
-                <div class="tab-ranking__title"></div>
-                <div class="tab-ranking__synopsis"></div>
+          <div class="flex-divider tab-ranking__divider flex flex-column flex--align flex--center">
+            <div class="tab-ranking__book" v-for="(book, index) in books" :key="index">
+              <div class="tab-ranking__item" v-if="index === 0"></div>
+              <div class="tab-ranking__item flex flex--align" v-else>
+                <div
+                  class="tab-ranking__index flex flex--align flex--around"
+                  :class="{'tab-ranking__index--second': index === 1,'tab-ranking__index--third':index===2}"
+                  v-text="index + 1"
+                ></div>
+                <div class="tab-ranking__cover">
+                  <img
+                    :src="`https://storage.googleapis.com/theta-images/${book._id.book[0].cover}`"
+                  >
+                </div>
+                <div class="tab-ranking__meta flex flex-column">
+                  <div class="tab-ranking__title" v-text="book._id.book[0].title"></div>
+                  <div class="tab-ranking__synopsis" v-text="book._id.book[0].synopsis"></div>
+                </div>
               </div>
             </div>
           </div>
@@ -60,10 +66,45 @@ export default {
   &__nav {
   }
   &__tab {
-    font-size: 18px;
+    font-size: 16px;
     padding: 10px;
     background-color: rgb(240, 240, 240);
     // background-image: linear-gradient(130deg, #a1c4fd, #c2e9fb);
+  }
+
+  &__cover {
+    img {
+      height: 118px;
+      width: 80px;
+    }
+    margin-top: 10px;
+  }
+  &__divider {
+    padding: 10px;
+  }
+  &__meta {
+    height: 118px;
+    margin-top: 10px;
+  }
+  &__index {
+    position: absolute;
+    height: 20px;
+    width: 20px;
+    top: 3px;
+    left: -5px;
+    font-size: 12px;
+    opacity: 0.9;
+    font-weight: bold;
+    &--second {
+      background-color: $silver;
+    }
+    &--third {
+      background-color: $bronze;
+    }
+    // background-color: black;
+  }
+  &__book {
+    position: relative;
   }
   &__first {
     margin-top: 10px;
@@ -72,11 +113,11 @@ export default {
 
     &__index {
       position: absolute;
-      height: 35px;
-      width: 35px;
+      height: 32px;
+      width: 32px;
       background-color: $gold;
-      top: -4px;
-      left: -4px;
+      top: -2px;
+      left: 0px;
       opacity: 0.9;
       font-size: 16px;
       font-weight: bold;
@@ -98,7 +139,7 @@ export default {
       width: 300px;
     }
     &__title {
-      font-size: 17px;
+      font-size: 18px;
     }
     &__synopsis {
       font-size: 16px;
