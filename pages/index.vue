0da110f9ac294134895261dcc90c29d05b798991
Arif Iwamoto <arif.iwamoto@gmail.com>
2019-02-24 17:03:01 +0900

    blob experiment

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="home-page">
     <div class="head-banner">
-      <!-- <img
+      <img
         src="http://gonakedselling.com/wp-content/uploads/2014/04/book-web-banner.jpg"
         style="width:100%;"
-      >-->
+      >
     </div>
     <div class="main-books">
-      <div class="card-title">
+      <div class="card-title flex flex--align">
         <h3>今日のおすすめ</h3>
+        <!-- <BlobOne></BlobOne> -->
+        <!-- <blob-two class="blob-back"></blob-two> -->
       </div>
       <BooksList :books="$store.state.book.books.highestrated"></BooksList>
       <ReviewList></ReviewList>
@@ -25,12 +27,16 @@
 import BooksList from "@/components/Homepage/BooksList";
 import ReviewList from "@/components/Homepage/ReviewList";
 import Ranking from "@/components/Homepage/Ranking";
+import BlobOne from "@/assets/svg/blob.svg";
+import BlobTwo from "@/assets/svg/blob2.svg";
 
 export default {
   components: {
     BooksList,
     ReviewList,
-    Ranking
+    Ranking,
+    BlobOne,
+    BlobTwo
   },
   // data() {
   //   return {
@@ -131,14 +137,26 @@ export default {
 .main-books {
 }
 
+.blob-back {
+  position: absolute;
+  z-index: -2;
+  // transform: rotate(90deg);
+  top: -70px;
+  left: -40px;
+  // fill: red;
+  opacity: 0.8;
+}
+
 .card-title {
   margin-top: 1rem;
+  position: relative;
   /* padding: 5px 0; */
   /* text-align: center; */
   /* background: #557fe980; */
   // width: 15rem;
-  border-radius: 0.5rem;
+  // border-radius: 0.5rem;
   font-weight: 300;
+  // height: 50px;
   /* -webkit-box-shadow: 1px 1px 1px 0px rgba(153, 153, 153, 0.75);
   -moz-box-shadow: 1px 1px 1px 0px rgba(153, 153, 153, 0.75);
   box-shadow: 1px 1px 1px 0px rgba(153, 153, 153, 0.75); */
@@ -149,6 +167,7 @@ h3 {
   font-size: 2rem;
   /* color: white; */
   margin: 0;
+  // margin-bottom: 10px;
   /* margin-top: 10px; */
   font-weight: 500;
 }
