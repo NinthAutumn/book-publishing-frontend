0048a170731e2c17f3087bedaf99fa4e8f75dc01
Arif Iwamoto <arif.iwamoto@gmail.com>
2019-02-26 23:16:50 +0900

    design prototype

--- components/LibraryPage/BookList.vue
+++ components/LibraryPage/BookList.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="library-booklist">
+    <div class="flex-divider flex flex--right">
+      <div class="library-booklist__sort flex flex--center flex--align">
+        <div class="library-booklist__sort__icon">
+          <fa icon="sort"></fa>
+        </div>
+        <div class="library-booklist__sort__text">並び替え</div>
+      </div>
+    </div>
     <ul class="library-booklist__list" v-if="trendings">
       <li class="library-booklist__item" v-for="book in books" :key="book.id">
         <Book :book="book._id.book[0]"></Book>
@@ -31,7 +39,25 @@ export default {
 <style lang="scss">
 .library-booklist {
   $self: &;
+  &__sort {
+    // background-color:;
+    box-shadow: 1px 1px 5px 0px rgb(233, 233, 233);
+    // border-radius: 5px;
+    width: 100px;
+    height: 30px;
 
+    &:hover {
+      cursor: pointer;
+      background-color: rgb(245, 245, 245);
+      transition: 200ms;
+    }
+    &__icon {
+      font-size: 14px;
+    }
+    &__text {
+      font-size: 14px;
+    }
+  }
   // grid-template-rows:
   &__list {
     display: grid;
--- pages/library/index.vue
+++ pages/library/index.vue
@@ -3,17 +3,9 @@
     <div class="library__header-wrapper flex flex--align">
       <header class="library__header">ライブラリー</header>
     </div>
+    <div class="library-bookmark__header">ブックマーク</div>
     <div class="flex-divider flex flex-row flex--between">
       <div class="library-bookmark">
-        <div class="library-bookmark__header">ブックマーク</div>
-        <div class="divider flex flex--right flex--align">
-          <div class="library-bookmark__sort-button flex flex--center flex--align">
-            <div class="library-bookmark__sort-button__icon">
-              <fa icon="sort"></fa>
-            </div>
-            <div class="library-bookmark__sort-button__text">並び替え</div>
-          </div>
-        </div>
         <BookList :trendings="true" :books="$store.state.ranking.ratings.total"></BookList>
       </div>
       <div class="library-profile">
@@ -31,6 +23,9 @@ export default {
     BookList,
     Profile
   },
+  methods: {
+    sortSelect(type) {}
+  },
   async fetch({ store }) {
     // await store.dispatch("book/getBook", params.id);
   }
@@ -47,25 +42,6 @@ export default {
     // width: 50%;
     margin-right: 10px;
 
-    &__sort-button {
-      // background-color:;
-      box-shadow: 1px 1px 5px 0px rgb(233, 233, 233);
-      // border-radius: 5px;
-      width: 100px;
-      height: 30px;
-
-      &:hover {
-        cursor: pointer;
-        background-color: rgb(245, 245, 245);
-        transition: 200ms;
-      }
-      &__icon {
-        font-size: 14px;
-      }
-      &__text {
-        font-size: 14px;
-      }
-    }
     &__header {
       font-size: 16px !important;
     }
