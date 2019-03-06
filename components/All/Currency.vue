94bdd9adcc90f8b8b503e00b6dbbb00b929f0134
Arif Iwamoto <arif.iwamoto@gmail.com>
2019-03-06 23:35:02 +0900

    currency design

--- components/All/Currency.vue
+++ components/All/Currency.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="currency flex flex--align">
     <div class="currency__number">{{amount}}</div>
-    <Crown class="currency__icon"></Crown>
+    <div class="currency__type">
+      <div class="currency__icon--back"></div>
+      <Crown class="currency__icon"></Crown>
+    </div>
   </div>
 </template>
 
@@ -20,23 +23,55 @@ export default {
 
 <style lang="scss">
 .currency {
+  position: relative;
   &__number {
     // font-family: Kameron;
     font-size: 16px;
-    color: $primary;
+    color: black;
     // font-weight: bold;
   }
+  &__type {
+    height: 20px;
+    width: 20px;
+    position: relative;
+    margin-left: 5px;
+  }
+  perspective: 400px;
+  perspective-origin: 100% 100%;
+  // perspective-origin-y: 100%;
+  // perspective-origin-x: 10%;
+
   &__icon {
-    fill: $primary;
+    position: absolute;
+    fill: $secondary;
     font-weight: bold;
     width: 20px;
     height: 20px;
-    margin-left: 5px;
+    // margin-left: 15px;
+
+    transition: 300ms;
+    // left: 24px;
+    transform: rotate3d(0, 1, 0, 40deg);
+    z-index: 1;
+    top: 1px;
+    &--back {
+      top: 1px;
+      transform: rotate3d(0, 1, 0, 40deg);
+      left: 1px;
+      position: absolute;
+      height: 20px;
+      width: 20px;
+      background-color: $secondary;
+      // box-shadow: 1px 1px 5px grey;
+      border-radius: 100px;
+      transition: 300ms;
+    }
+
     // box-shadow: inset 1px 1px 5px rgb(253, 225, 225),
     //   1px 1px 5px rgb(255, 241, 241);
     // box-shadow: 1px 1px 5px grey;
     border-radius: 100px;
-    background-color: #f4648a6b;
+    background-color: #ffffff;
   }
 }
 </style>
