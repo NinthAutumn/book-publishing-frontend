cc6fec5bf40580adf8846e0d2d9223105fb320e8
Arif Iwamoto <arif.iwamoto@gmail.com>
2019-04-04 00:40:18 +0900

    ranking more options

---
+++ components/User/Nav.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="user-nav">
+    <div class="user-nav__container">
+      <div class="user-nav__avatar"></div>
+      <div class="user-nav__content">
+        <ul class="user-nav__stats-list">
+          <li class="user-nav__stats-item"></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+</style>
---
+++ pages/users/_id.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="user-profile"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+</style>
